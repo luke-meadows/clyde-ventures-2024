d45fa6088ab522d91e81670347b11ca8ed60c6c0
luke-meadows <lukemeadowsdev@gmail.com>
2024-02-02 23:03:31 +0000

    framer-motion

---
+++ app/lib/framer-motion.js
@@ -0,0 +1,43 @@
+export const dropdownFramerVariant = {
+  static: {
+    // y: '0',
+    opacity: 1,
+  },
+  initial: {
+    // y: '-100%',
+    opacity: 0,
+  },
+  enter: {
+    opacity: 1,
+    // y: '0',
+    zIndex: '-1',
+    transition: {
+      duration: 0.2,
+      when: 'beforeChildren',
+    },
+  },
+  exit: {
+    zIndex: '-1',
+    // y: '-100%',
+    opacity: 0,
+    transition: { duration: 0.3 },
+  },
+};
+
+export const dropdownInnerFramerVariant = {
+  initial: {
+    opacity: '0',
+  },
+  enter: {
+    opacity: 1,
+    transition: {
+      duration: 0.1,
+      delay: 0,
+      when: 'afterParent',
+    },
+  },
+  exit: {
+    opacity: '0',
+    transition: { duration: 0 },
+  },
+};
---
+++ components/AnimationWrapper.js
@@ -0,0 +1,19 @@
+'use client';
+import { motion } from 'framer-motion';
+export default function AnimationWrapper({ children }) {
+  const variants = {
+    hidden: { x: 0, y: 100 },
+    enter: { x: 0, y: 0 },
+  };
+  return (
+    <motion.main
+      variants={variants}
+      initial="hidden"
+      animate="enter"
+      transition={{ type: 'ease', duration: 0.5 }}
+      className="mt-24"
+    >
+      {children}
+    </motion.main>
+  );
+}
