134861364f9172f25682bac8c5561164a464fbda
luke-meadows <lukemeadowsdev@gmail.com>
2024-02-02 23:02:31 +0000

    config & clean up

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,31 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+      },
+      colors: {
+        'primary-orange': '#F9C157',
+        'primary-blue': '#032E61',
+        'super-light-grey': '#ECECEC',
+        'off-white': '#f8f8f8',
+        'light-grey': '#484848',
+        'medium-grey': '#464646',
+        'dark-grey': '#404040',
+        'linkedin-blue': '#0A66C2',
+        'black-text': '#1B1B1B',
+        'grey-text': '#464646',
+        'card-bg': '#F8F8F8',
+      },
+      boxShadow: {
+        'nav-bottom': '0 20px 20px -20px rgba(0, 0, 0, 0.3)',
+        bottom: '0 20px 20px -20px rgba(0, 0, 0, 0.5)',
+        // card: '0px 15px 15px -15px rgba(0, 0, 0, 0.5)',
+        card: 'rgba(0, 0, 0, 0.35) 0px 5px 15px;',
+        button: '0px 15px 15px -15px rgba(0, 0, 0, 0.5)',
+      },
+    },
   },
   plugins: [],
 };
