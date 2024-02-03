0a4921ef833a61c6fd9f3faa19f07b5cac2750aa
luke-meadows <lukemeadowsdev@gmail.com>
2024-02-03 16:03:44 +0000

    NavDropdown: framer motion variant

--- app/lib/framer-motion.js
+++ app/lib/framer-motion.js
@@ -41,3 +41,19 @@ export const dropdownInnerFramerVariant = {
     transition: { duration: 0 },
   },
 };
+export const dropdownBackgroundFramerVariant = {
+  initial: {
+    opacity: '0',
+  },
+  enter: {
+    opacity: 0.8,
+    transition: {
+      duration: 0.1,
+      delay: 0,
+    },
+  },
+  exit: {
+    opacity: '0',
+    transition: { duration: 0 },
+  },
+};
