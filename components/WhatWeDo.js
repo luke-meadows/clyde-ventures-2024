4efd78f646f89e68c93ba0dde8484f9740607373
luke-meadows <lukemeadowsdev@gmail.com>
2024-02-03 00:34:06 +0000

    WhatWeDo: Service links to service page

--- components/WhatWeDo.js
+++ components/WhatWeDo.js
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+
 export default function WhatWeDo() {
   const whatWeDoCards = [
     {
@@ -8,6 +9,72 @@ export default function WhatWeDo() {
       subtitle:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ex ratione et nulla odio a fuga, dolore distinctio qui porro dolorum quidem harum labore magni corrupti obcaecati molestias totam atque.',
       link: '/services',
+      options: [
+        {
+          title: 'Sales Strategy',
+          url: '/services/consultancy/top-line-growth',
+        },
+        {
+          title: 'Debt Strategy',
+          url: '/services/consultancy/top-line-growth',
+        },
+        {
+          title: 'Margin Optimisation',
+          url: '/services/consultancy/top-line-growth',
+        },
+        {
+          title: 'Sales Strategy',
+          url: '/services/consultancy/top-line-growth',
+        },
+        {
+          title: 'Contact Strategy',
+          url: '/services/consultancy/service-excellence',
+        },
+        {
+          title: 'Complaints Strategy',
+          url: '/services/consultancy/service-excellence',
+        },
+        {
+          title: 'Vulnerability Strategy',
+          url: '/services/consultancy/service-excellence',
+        },
+        {
+          title: 'Existing Process Optimisation',
+          url: '/services/consultancy/service-excellence',
+        },
+        {
+          title: 'OPEX reduction & Lean',
+          url: '/services/consultancy/opex-optimisation',
+        },
+        {
+          title: 'RPA, Automation & AI',
+          url: '/services/consultancy/opex-optimisation',
+        },
+        {
+          title: 'Data & Analytics',
+          url: '/services/consultancy/opex-optimisation',
+        },
+        {
+          title: 'Business Care & ROI Due Diligence',
+          url: '/services/consultancy/opex-optimisation',
+        },
+        {
+          title: 'Service & TOM Design',
+          url: '/services/consultancy/business-and-culture-change',
+        },
+        {
+          title: 'Adoption & Engagement',
+          url: '/services/consultancy/business-and-culture-change',
+        },
+        {
+          title: 'Culture Change',
+          url: '/services/consultancy/business-and-culture-change',
+        },
+        {
+          title: 'Social Mobility D,E & I',
+          url: '/services/consultancy/business-and-culture-change',
+        },
+      ],
     },
     {
       title: 'Integration',
@@ -15,6 +82,72 @@ export default function WhatWeDo() {
       subtitle:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ex dolorum quidem harum labore magni   ratione et nulla odio a Id ex ratione et nulla odio a Id ex ratione et nulla odio a.Id ex ratione et nulla odio a.',
       link: '/services/integration',
+      options: [
+        {
+          title: 'Sales Cloud',
+          url: '/services/consultancy/top-line-growth-salesforce',
+        },
+        {
+          title: 'Marketing Cloud',
+          url: '/services/consultancy/top-line-growth-salesforce',
+        },
+        {
+          title: 'Energy & Utilities Cloud',
+          url: '/services/consultancy/top-line-growth-salesforce',
+        },
+        {
+          title: 'Account Engagement',
+          url: '/services/consultancy/top-line-growth-salesforce',
+        },
+        {
+          title: 'Service Cloud',
+          url: '/services/consultancy/top-line-growth-salesforce',
+        },
+        {
+          title: 'Experience Cloud',
+          url: '/services/consultancy/top-line-growth-salesforce',
+        },
+        {
+          title: 'Product Design',
+          url: '/services/consultancy/top-line-growth-salesforce',
+        },
+        {
+          title: 'Field Service Cloud',
+          url: '/services/integration/field-optimisation-salesforce',
+        },
+        {
+          title: 'Service Cloud',
+          url: '/services/integration/field-optimisation-salesforce',
+        },
+        {
+          title: 'Experience Cloud',
+          url: '/services/integration/field-optimisation-salesforce',
+        },
+        {
+          title: 'Product Design',
+          url: '/services/integration/field-optimisation-salesforce',
+        },
+        {
+          title: 'ODI Integration & Design',
+          url: '/services/integration/wacc-outperformance',
+        },
+        {
+          title: 'Value Modelling',
+          url: '/services/integration/wacc-outperformance',
+        },
+        {
+          title: 'Business Case & ROI Due Diligence',
+          url: '/services/integration/wacc-outperformance',
+        },
+        {
+          title: 'Design Assurance',
+          url: '/services/integration/independence-upskilling-design-authority',
+        },
+        {
+          title: 'Client Capability Cultivation',
+          url: '/services/integration/independence-upskilling-design-authority',
+        },
+      ],
     },
     {
       title: 'Education',
@@ -22,6 +155,9 @@ export default function WhatWeDo() {
       subtitle:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Id ex ratione et dolorum quidem harum labore magni  nulla odio a fuga, dolore distinctio qui porro dolorum quidem harum labore magni corrupti',
       link: '/services',
+      options: [
+        { title: 'Sustainable Futures MBA', url: '/services/education/MBA' },
+      ],
     },
     {
       title: 'Manufacturing',
@@ -29,6 +165,12 @@ export default function WhatWeDo() {
       subtitle:
         'Id ex ratione et nulla odio a fuga, dolore distinctio qui porro dolorum quidem harum labore magni corrupti obcaecati molestias totam atque.',
       link: '/services',
+      options: [
+        {
+          title: 'Fire Resistant Panels',
+          url: '/services/manufacturing/FRP',
+        },
+      ],
     },
   ];
   return (
@@ -45,35 +187,48 @@ export default function WhatWeDo() {
       <div className="grid grid-cols-2 gap-6">
         {whatWeDoCards.map((card, i) => {
           return (
-            // Container
-            <div
-              key={i}
-              className="shadow-card flex-1 card-rounding bg-black-text"
-            >
-              {/* Card Top */}
-              <div className="card-top flex justify-between items-center px-3 pt-3">
-                <div className="card-top-left flex items-center gap-4">
-                  <div className="bg-primary-orange h-14 w-14 aspect-square center-contents rounded-full text-lg cursor-pointer">
-                    <Image src={card.icon} width={30} height={30} />
+            <div>
+              {/* // Container */}
+              <div
+                key={i}
+                className="shadow-card flex-1 card-rounding bg-black-text py-6"
+              >
+                {/* Card Top */}
+                <div className="card-top flex justify-between items-center px-6">
+                  <div className="card-top-left flex items-center gap-4">
+                    <div className="bg-primary-orange h-14 w-14 aspect-square center-contents rounded-full text-lg cursor-pointer">
+                      <Image src={card.icon} width={30} height={30} />
+                    </div>
+                    <h3 className="font-semibold text-xl text-white">
+                      {card.title}
+                    </h3>
                   </div>
-                  <h3 className="font-semibold text-xl text-white">
-                    {card.title}
-                  </h3>
+                  <div className="card-top-right">
+                    <Link href={card.link}>
+                      <button className="bg-primary-orange text-black-text pl-6 pr-4 h-10 rounded-full min-w-fit w-fit mx-auto shadow-button text-xs font-semibold">
+                        Learn More <i className="icon-angle-right" />
+                      </button>
+                    </Link>
+                  </div>
                 </div>
-                <div className="card-top-right">
-                  <Link href={card.link}>
-                    <button className="bg-primary-orange text-dark-grey pl-6 pr-4 h-10 rounded-full min-w-fit w-fit mx-auto shadow-button text-xs font-semibold">
-                      Learn More <i className="icon-angle-right" />
-                    </button>
-                  </Link>
-                </div>
-              </div>
 
-              {/* Card Bottom */}
-              <div className="p-4">
-                <p className="text-sm font-normal mb-2 text-off-white">
-                  {card.subtitle}
-                </p>
+                {/* Card Bottom */}
+                <div className="px-6 ">
+                  <p className="text-sm font-normal text-off-white my-5">
+                    {card.subtitle}
+                  </p>
+                  <div className="grid grid-cols-2 text-off-white gap-x-2 gap-y-3 ">
+                    {card.options.map((service, i) => (
+                      <Link
+                        href={service.url}
+                        key={i}
+                        className="text-sm border-[1px] py-1 px-3 rounded-full hover:bg-primary-orange hover:text-black-text hover:border-primary-orange cursor-pointer font-normal hover:font-medium center-contents text-center"
+                      >
+                        {service.title}
+                      </Link>
+                    ))}
+                  </div>
+                </div>
               </div>
             </div>
           );
