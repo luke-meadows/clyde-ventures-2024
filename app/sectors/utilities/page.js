7fb73f6fc529bbdb6ad829b67dc6eefd38767540
luke-meadows <lukemeadowsdev@gmail.com>
2024-04-24 14:21:58 +0100

    sector pages

--- app/sectors/utilities/page.js
+++ app/sectors/utilities/page.js
@@ -1,11 +1,13 @@
 import AnimationWrapper from '@components/AnimationWrapper';
 import MainHero from '@components/MainHero';
 import WhyChooseClyde from '@components/WhyChooseClyde';
+import SectorIntroSection from '@components/sectors/SectorIntroSection';
 
 export default function UtilitiesPage() {
   return (
     <AnimationWrapper>
       <MainHero hero="utilities"></MainHero>
+      <SectorIntroSection />
       <div className="page-break" />
       <WhyChooseClyde title="Why Choose Clyde Heading Placeholder" />
       <div className="page-break" />
--- components/sectors/SectorIntroSection.js
+++ components/sectors/SectorIntroSection.js
@@ -1,10 +1,34 @@
+import SolutionsGrid from '@components/SolutionsGrid';
+import OutstandingCareItem from '@components/outstanding-customer-care/OutstandingCareItem';
+
 export default function SectorIntroSection({ sector }) {
   const sectors = {
     utilities: {
       title: 'Practical Solutions Tailored for Utility Enterprises',
       blurb:
-        'Most of the Clyde Ventures team have their roots in the Utility sector, we understand the operation at its foundation enabling us to tailor solutions to your needs. Partnering with us grants you access to a team of seasoned professionals, adept at crafting systems that harmonize with meeting the challenges of a modern Utility company.',
+        'Most of the Clyde Ventures team have their roots in the Utility sector, we understand the operation at its foundation enabling us to tailor solutions to your needs. Partnering with us grants you access to a team of seasoned professionals, adept at crafting systems that harmonise with meeting the challenges of a modern Utility company.',
     },
   };
-  return <div></div>;
+  return (
+    <>
+      <div className="inner-container text-center">
+        <h2 className="section-heading">{sectors.utilities.title}</h2>
+        <p className="section-subheading">{sectors.utilities.blurb}</p>
+        <br />
+        <h2 className="section-heading">Industry Pain Points</h2>
+        <br />
+        <div className="grid grid-cols-2 text-left">
+          <OutstandingCareItem item="industry" />
+          <OutstandingCareItem item="industry" />
+          <OutstandingCareItem item="industry" />
+          <OutstandingCareItem item="industry" />
+        </div>
+        <br />
+
+        <h2 className="section-heading">Clyde Ventures Utility Solutions</h2>
+        <br />
+        <SolutionsGrid solution="salesforce" />
+      </div>
+    </>
+  );
 }
