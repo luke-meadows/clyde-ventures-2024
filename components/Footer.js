84315e1d5d129fa7b106c0be8670bd4c60e69ff7
luke-meadows <lukemeadowsdev@gmail.com>
2024-04-25 10:36:52 +0100

    Footer links

--- components/Footer.js
+++ components/Footer.js
@@ -5,9 +5,9 @@ import Image from 'next/image';
 export default function Footer() {
   return (
     <div>
-      <div className="bg-black text-white pt-16 pb-8 ">
+      <div className="bg-black text-white pt-16">
         <div className="inner-container">
-          <div className="grid grid-cols-3 gap-16">
+          <div className="grid grid-cols-4 gap-16">
             <div>
               <h5 className="mb-4">Contact Us</h5>
               <ul className="text-xs font-normal flex flex-col gap-1">
@@ -37,9 +37,17 @@ export default function Footer() {
                 <Link href="/">Education</Link>
               </ul>
             </div>
+            <div>
+              <h5 className="mb-4">Policies</h5>
+              <ul className="text-xs font-normal flex flex-col gap-1">
+                <Link href="/">Privacy Statement</Link>
+                <Link href="/">Modern Slavery Statement</Link>
+                <Link href="/">Carbon Accounting</Link>
+              </ul>
+            </div>
           </div>
-          <div className="flex justify-between items-end mt-6">
-            <div className="logos flex gap-4 items-center">
+          <div className="flex justify-between items-center mt-6">
+            <div className="logos flex items-center">
               {/* <div className="bg-white p-1 rounded flex items-center justify-center h-fit">
                 <Image
                   src="/images/logo.png"
@@ -49,28 +57,31 @@ export default function Footer() {
                 />
               </div> */}
               <Image
-                src="/images/sf-partner-logo-rectangle.png"
+                src="/images/sf-partner-logo-square.png"
                 height={110}
-                width={185}
+                width={80}
                 className="object-contain mr-2"
               />
               <Image
                 src="/images/accreditations-partners/CfA Cert Logo White UKAS ISO 9001.png"
-                height={40}
-                width={120}
+                height={50}
+                width={158}
               />
               <Image
                 src="/images/accreditations-partners/CfA Cert Logo White UKAS ISO 14001.png"
-                height={40}
-                width={120}
+                height={50}
+                width={158}
               />
               <Image
                 src="/images/accreditations-partners/CfA Cert Logo White UKAS ISO 27001.png"
-                height={40}
-                width={120}
+                height={50}
+                width={158}
               />
             </div>
-            <div className="linkedin flex gap-6">
+            <div className="linkedin items-center flex gap-2">
+              <div className="icon-wrapper bg-primary-orange">
+                <i className="icon-mail text-black" />
+              </div>
               <a
                 href="https://www.linkedin.com/company/clyde-ventures"
                 target="blank"
@@ -79,6 +90,13 @@ export default function Footer() {
                 <i className="icon-linkedin" />
               </a>
             </div>
+          </div>
+        </div>
+        <div className="border-gray-500 border-t-[1px] py-2">
+          <div className="inner-container">
+            <p className="text-xs font-light">
+              © Clyde Ventures 2022 | Registered in Scotland: SC723600
+            </p>
           </div>
         </div>
       </div>
