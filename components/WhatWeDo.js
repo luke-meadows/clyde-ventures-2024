377bebe21bae13815ae2f5553be2bbae53e8a41b
luke-meadows <lukemeadowsdev@gmail.com>
2024-04-30 15:03:25 +0100

    font-changes

--- components/WhatWeDo.js
+++ components/WhatWeDo.js
@@ -5,17 +5,19 @@ import DropdownGrid from './nav/DropdownGrid';
 
 export default function WhatWeDo() {
   return (
-    <div className="inner-container">
-      <h2 className="section-heading text-center text-black-text">
-        What we do
-      </h2>
-      <p className="section-subheading">
-        We provide expertise with innovation to empower businesses navigating
-        the dynamic landscape of technology integration, sustainable energy and
-        business growth. Find out how Clyde Ventures can transform your
-        organisation below.
-      </p>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-6">
+    <div>
+      <div className="inner-container">
+        <h2 className="section-heading text-center text-black-text">
+          What we do
+        </h2>
+        <p className="section-subheading">
+          We provide expertise with innovation to empower businesses navigating
+          the dynamic landscape of technology integration, sustainable energy
+          and business growth. Find out how Clyde Ventures can transform your
+          organisation below.
+        </p>
+      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 pb-6 md:pb-0 gap-6 max-w-[1300px] mx-auto md:px-8 bg-black-text md:bg-white">
         {dropdownDirectory.services.map((item, i) => (
           <DropdownGrid item={item} key={i} />
         ))}
